cfba6158d4616d0608c2da46610c492b1a383926
pradeep2002 <67775068+pradeep2002@users.noreply.github.com>
2020-08-02 21:01:27 +0530

    accounts

---
+++ accounts.py
@@ -0,0 +1,16 @@
+#create your own mail id's  and password before using this code.
+
+patients = {
+    '1' : ['patient_1@hospital.com', 'patient_1_password'],
+    '2' : ['patient_2@hospital.com', 'patient_2_password'],
+    '3' : ['patient_3@hospital.com', 'patient_3_password'],
+    '4' : ['patient_4@hospital.com', 'patient_4_password']
+}
+
+doctors = {
+    'doctor_1' : ['doctor_1@hospital.com'],
+    'doctor_2' : ['doctor_2@hospital.com'],
+    'doctor_3' : ['doctor_3@hospital.com'],
+    'doctor_4' : ['doctor_4@hospital.com'],
+    'default_doctor' : ['default_doctor@hospital.com']
+}
